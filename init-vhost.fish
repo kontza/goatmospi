7402b0e7d067dbe22a86834488f2a093a4b29733
Rumpu-Jussi <kontza@gmail.com>
2016-04-19 17:18:38 +0300

    Replaced vhost-init with a Fish-version.

--- init-vhost.fish
+++ init-vhost.fish
@@ -1,18 +1,16 @@
 #!/bin/bash
-__ATMOSPI_STATIC="$(pwd)/web/static"
-__ATMOSPI_PREFIX="goatmospi"
-__CONF_NAME="000-$__ATMOSPI_PREFIX.conf"
-rm 000-atmospi.conf >& /dev/null
-cat << EOF > $__CONF_NAME
-<VirtualHost *:80>
+set -l __ATMOSPI_STATIC "$PWD/web/static"
+set -l __ATMOSPI_PREFIX (basename $PWD)
+set -l __CONF_NAME "000-$__ATMOSPI_PREFIX.conf"
+rm $__CONF_NAME ^&1
+echo -n "<VirtualHost *:80>
     ProxyRequests Off
     <Location /$__ATMOSPI_PREFIX>
-        RequestHeader    set Atmospi-Prefix-Path "$__ATMOSPI_PREFIX"
+        RequestHeader    set Atmospi-Prefix-Path \"$__ATMOSPI_PREFIX\"
         ProxyPass        http://localhost:4002
         ProxyPassReverse http://localhost:4002
     </Location>
-</VirtualHost>
-EOF
+</VirtualHost>" > $__CONF_NAME
 echo Next:
 echo "sudo ln -sf \$PWD/$__CONF_NAME /etc/apache2/sites-enabled/"
 echo "sudo apache2ctl restart"
