6d28ee1eeab4c1a50c92a44fccf6e329315fa8dd
Rumpu-Jussi <kontza@gmail.com>
2016-04-19 20:46:55 +0300

    Data tester script updated.

--- data-tests.sh
+++ data-tests.sh
@@ -1,4 +1,14 @@
-curl localhost/goatmospi/data/devices/temperature
+echo "localhost/goatmospi/data/devices/temperature"
+curl -H "Accept-Encoding: application/json" localhost/goatmospi/data/devices/temperature
+sleep 0.5
+echo "
+localhost/goatmospi/data/devices/humidity"
 curl localhost/goatmospi/data/devices/humidity
+sleep 0.5
+echo "
+localhost/goatmospi/data/latest/temperature"
 curl localhost/goatmospi/data/latest/temperature
+sleep 0.5
+echo "
+localhost/goatmospi/data/latest/humidity"
 curl localhost/goatmospi/data/latest/humidity
