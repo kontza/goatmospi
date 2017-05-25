86ea0d7ae6c3f528ffa74821a874b302a6013070
Rumpu-Jussi <kontza@gmail.com>
2017-05-25 21:44:01 +0300

    Refactoring YAML loading.

--- cfg.go
+++ cfg.go
@@ -10,21 +10,27 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-// App config.
+// ApplicationConfig contains the application configuration.
 type ApplicationConfig struct {
-	Address string `yaml:"address"`
-	Port    string `yaml:"port"`
-	Client  struct {
-		RangeSeconds    string `yaml:"range_seconds"`
-		Precision       string `yaml:"precision"`
-		TemperatureUnit string `yaml:"t_unit"`
-	}
-	Database struct {
-		Hostname string `yaml:"hostname"`
-		Name     string `yaml:"name"`
-		User     string `yaml:"user"`
-		Password string `yaml:"password"`
-	} `yaml:"db"`
+	Address string `yaml:"address,omitempty"`
+	Port    string `yaml:"port,omitempty"`
+	Client
+	Database `yaml:"db"`
+}
+
+// Client is the inner struct for a client configuration.
+type Client struct {
+	RangeSeconds    string `yaml:"range_seconds,omitempty"`
+	Precision       string `yaml:"precision,omitempty"`
+	TemperatureUnit string `yaml:"t_unit,omitempty"`
+}
+
+// Database is the inner struct for DB configuration.
+type Database struct {
+	Hostname string `yaml:"hostname,omitempty"`
+	Name     string `yaml:"name,omitempty"`
+	User     string `yaml:"user,omitempty"`
+	Password string `yaml:"password,omitempty"`
 }
 
 // command line flags
@@ -35,9 +41,25 @@ var (
 		File    string `short:"f" long:"file" default:"goatmospi.yml" description:"The configuration file (YAML) to use for the operations." value-name:"FILE"`
 	}
 	logger    = loggo.GetLogger("goatmospi")
-	appConfig ApplicationConfig
+	appConfig = ApplicationConfig{
+		"127.0.0.1", "4002",
+		Client{
+			"604800",
+			"2",
+			"C",
+		},
+		Database{
+			"localhost",
+			"dbname",
+			"dbuser",
+			"password",
+		},
+	}
 )
 
+/**
+Read application config from the YAML.
+*/
 func readConfig() ApplicationConfig {
 	filename, _ := filepath.Abs(options.File)
 	yamlFile, err := ioutil.ReadFile(filename)
@@ -54,10 +76,43 @@ func readConfig() ApplicationConfig {
 
 	printValue := _config
 	printValue.Database.Password = "********"
-	logger.Infof("Read YAML-config: %+v", printValue)
+	logger.Infof("Read config: %+v", printValue)
+	/*
+		Sanitize values.
+	*/
+	if _config.Address == "" {
+		_config.Address = appConfig.Address
+	}
+	if _config.Port == "" {
+		_config.Port = appConfig.Port
+	}
+	if _config.Client.RangeSeconds == "" {
+		_config.Client.RangeSeconds = appConfig.Client.RangeSeconds
+	}
+	if _config.Client.Precision == "" {
+		_config.Client.Precision = appConfig.Client.Precision
+	}
+	if _config.Client.TemperatureUnit == "" {
+		_config.Client.TemperatureUnit = appConfig.Client.TemperatureUnit
+	}
+	if _config.Database.Hostname == "" {
+		_config.Database.Hostname = appConfig.Database.Hostname
+	}
+	if _config.Database.Name == "" {
+		_config.Database.Name = appConfig.Database.Name
+	}
+	if _config.Database.User == "" {
+		_config.Database.User = appConfig.Database.User
+	}
+	if _config.Database.Password == "" {
+		_config.Database.Password = appConfig.Database.Password
+	}
 	return _config
 }
 
+/**
+Main routine.
+*/
 func main() {
 	if _, err := flags.Parse(&options); err != nil {
 		if flagsErr, ok := err.(*flags.Error); ok && flagsErr.Type == flags.ErrHelp {
@@ -74,6 +129,7 @@ func main() {
 	if options.Version {
 		println("Goatmospi v1.0")
 	} else {
-		readConfig()
+		appConfig = readConfig()
+		logger.Infof("Initial YAML-config: %+v", appConfig)
 	}
 }
